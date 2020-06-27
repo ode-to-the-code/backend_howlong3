4e87c90ba806e660eed997986c25dd528df1d9b6
github username <github email address>
2020-06-26 18:56:58 -0700

    user model, user migration and user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 
+#
 # events = Event.create([
 #   {name: "Start of Semester", description: "when I started this school year", startTime: 08102020},
 #   {name: "Got My Dog", description: "at the pet store on 45th and Lex", startTime: 05022017},
@@ -15,3 +15,6 @@
 
 event1 = Event.create(name: "Start of Semester", description: "when I started this school year", startTime: 08102020)
 event2 = Event.create(name: "Got My Dog", description: "at the pet store on 45th and Lex", startTime: 05022017)
+
+user1 = User.create(name: "Ames Jameson")
+user2 = User.create(name: "Delilah Amara")
